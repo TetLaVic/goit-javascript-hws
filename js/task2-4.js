bf4471008486c06032bcef45722bc6b195ea2e4d
Tetiana Lavrenko <tetiana.lavrenko@thu.de>
2020-10-04 22:29:14 +0200

    completed homework 2

---
+++ js/task2-4.js
@@ -0,0 +1,39 @@
+// function formatString(string, maxLength = 40) {
+//   // Write code under this line
+//   if (string.length <= maxLength) {
+//     return string;
+//   }
+
+//   return string.slice(0, maxLength) + "...";
+// }
+
+// console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// // 'Curabitur ligula sapien, tincidunt non.'
+
+// console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// // 'Vestibulum facilisis, purus nec pulvinar...'
+
+// console.log(
+//   formatString("Vestibulum facilisis, purus nec pulvinar iaculis.", 30)
+// );
+// 'Vestibulum facilisis, purus ne...'
+
+// OR
+
+function formatString(string, maxLength = 40) {
+  // Write code under this line
+  const formattedString =
+    string.length <= maxLength ? string : string.slice(0, maxLength) + "...";
+
+  return formattedString;
+}
+
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// 'Curabitur ligula sapien, tincidunt non.'
+
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// 'Vestibulum facilisis, purus nec pulvinar...'
+
+console.log(
+  formatString("Vestibulum facilisis, purus nec pulvinar iaculis.", 30)
+);
